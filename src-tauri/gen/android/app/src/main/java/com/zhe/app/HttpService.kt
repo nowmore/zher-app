2888c31eb4d9a4a4b607455b181ba8525bad1832
w3n9 <9102zyx@gmail.com>
2025-12-02 14:56:54 +0800

    remove barcode scan support;add foregroud service;

---
+++ src-tauri/gen/android/app/src/main/java/com/zhe/app/HttpService.kt
@@ -0,0 +1,42 @@
+package com.zhe.app
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+
+class HttpService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        createNotificationChannel()
+
+        val notification: Notification = NotificationCompat.Builder(this, "HttpServiceChannel")
+            .setContentTitle("HTTP Service Running")
+            .setContentText("Server is listening in background...")
+            .setSmallIcon(android.R.drawable.ic_menu_info_details) // 替换为你的图标
+            .build()
+
+        startForeground(1, notification)
+
+        return START_STICKY
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                "HttpServiceChannel",
+                "HTTP Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(serviceChannel)
+        }
+    }
+}
